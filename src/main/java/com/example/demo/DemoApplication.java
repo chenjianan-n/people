0a28f336e5d8f366058d60de6017ce7f3f49c4e8
[chenjianan] <1406971327@qq.com>
2020-06-29 19:45:03 +0800

    陈

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("陈迦南");
     }
 
 }
